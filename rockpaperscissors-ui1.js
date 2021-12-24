43022b2030339f17b7c27168dd06966ac6048276
derek <nashintono@gmail.com>
2021-12-23 23:08:13 -0800

    Added in DOM manipulation to take commands out of the terminal

--- rockpaperscissors-ui1.js
+++ rockpaperscissors-ui1.js
@@ -1,12 +1,18 @@
-// game variables
-let rock = "rock";
-let paper = "paper";
-let scissors = 'scissors';
+const p1 = document.querySelector('#p1');
+const compChoice = document.querySelector('#compChoice');
+const playerChoice = document.querySelector('#playerChoice');
+const gameResults = document.querySelector('#gameResults')
+
+////////////////////
+//     Logic      //
+////////////////////
+
+//variable init
+let playerSelection = "";
 
 //Computer chooses rock paper or scissors here
 function computerPlay(choice) {
     randomNum = Math.floor(Math.random() * 3);
-    console.log(randomNum)
     if (randomNum == 0) {
         return "rock";
     } else if (randomNum == 1) {
@@ -16,27 +22,52 @@ function computerPlay(choice) {
     }
 }
 
+//Listens for event listener for playerSelection
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    playerSelection = "rock";
+    game();
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    playerSelection = "paper";
+    game();
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    playerSelection = "scissors";
+    game();
+});
+
+
 //Compares the players selection to the computers and prints the result
 function playRound (playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You lose! Paper beats Rock");
+        gameResults.textContent = "You lose! Paper beats Rock";
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You win! Rock beats Scissors");
+        gameResults.textContent = "You win! Rock beats Scissors";
     } else if (playerSelection == "rock" && computerSelection == "rock"){
-        console.log("You've tied, Rock and Rock!");
+        gameResults.textContent = "You've tied, Rock and Rock!";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log("You lose! Scissors beat Paper!");
+        gameResults.textContent = "You lose! Scissors beat Paper!";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You win! Paper beats Rock");
+        gameResults.textContent = "You win! Paper beats Rock";
     } else if (playerSelection == "paper" && computerSelection == "paper"){
-        console.log("You've tied, Paper and Paper!");
+        gameResults.textContent = "You've tied, Paper and Paper!";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("You lose! Rock beats Scissors!");
+        gameResults.textContent = "You lose! Rock beats Scissors!";
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You win! Scissors beats Paper!");
+        gameResults.textContent = "You win! Scissors beats Paper!";
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        console.log("You've tied! Scissors and Scissors!");
+        gameResults.textContent = "You've tied! Scissors and Scissors!";
     }
 };
 
-game();
+function game() {
+    const computerSelection = computerPlay();
+    playerChoice.textContent = `You chose ${playerSelection}`;
+    compChoice.textContent = `The computer chose ${computerSelection}`;
+    playRound(playerSelection, computerSelection);
+}
