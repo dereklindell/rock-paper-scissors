5ca63b5f77413fc5a79d6f0d4d1154e1346184f3
derek <nashintono@gmail.com>
2022-01-01 15:23:28 -0800

    Finished the skeleton of the UI and logic

--- rockpaperscissors-ui1.js
+++ rockpaperscissors-ui1.js
@@ -1,8 +1,11 @@
 const p1 = document.querySelector('#p1');
 const compChoice = document.querySelector('#compChoice');
 const playerChoice = document.querySelector('#playerChoice');
-const roundResults = document.querySelector('#roundResults')
-const gameResults = document.querySelector('#gameResults')
+const roundResults = document.querySelector('#roundResults');
+const gameResults = document.querySelector('#gameResults');
+const playerScoreUI = document.querySelector('#playerScore');
+const computerScoreUI = document.querySelector('#computerScore');
+const welcome = document.querySelector('#welcome')
 
 ////////////////////
 //     Logic      //
@@ -44,52 +47,102 @@ scissors.addEventListener('click', () => {
     game();
 });
 
+const resetUI = document.querySelector('#reset');
+resetUI.addEventListener('click', () =>  {
+    reset();
+})
+
 
 //Compares the players selection to the computers and prints the result
 function playRound (playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "paper") {
         roundResults.textContent = "Computer gains a point! Paper beats Rock";
-        console.log(computerScore++);
+        computerScore++;
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         roundResults.textContent = "You gained a point! Rock beats Scissors";
         playerScore++;
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
     } else if (playerSelection == "rock" && computerSelection == "rock"){
         roundResults.textContent = "You've tied, Rock and Rock!";
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         roundResults.textContent = "Computer gains a point! Scissors beat Paper!";
-        console.log(computerScore++);
+        computerScore++;
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         roundResults.textContent = "You gained a point! Paper beats Rock";
         playerScore++;
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
     } else if (playerSelection == "paper" && computerSelection == "paper"){
         roundResults.textContent = "You've tied, Paper and Paper!";
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         roundResults.textContent = "Computer gains a point! Rock beats Scissors!";
-        console.log(computerScore++);
+        computerScore++;
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         roundResults.textContent = "You've gained a point'! Scissors beats Paper!";
         playerScore++;
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
         roundResults.textContent = "You've tied! Scissors and Scissors!";
+
+        playerScoreUI.textContent = `Player Score: ${playerScore}`;
+        computerScoreUI.textContent = `Computer Score: ${computerScore}`;
+
     }
 };
 
+//game function to check for player score and if any users have won or not
 function game() {
-    if (playerScore <= 5 && computerScore <= 5) {
+    if (playerScore <= 4 && computerScore <= 4) {
         const computerSelection = computerPlay();
         playerChoice.textContent = `You chose ${playerSelection}`;
         compChoice.textContent = `The computer chose ${computerSelection}`;
         playRound(playerSelection, computerSelection);
+        welcome.textContent = ``;
     } else {
         if (playerScore == 5) {
             gameResults.textContent = "You've won!"
         } else if (computerScore == 5) {
             gameResults.textContent = "The computer won!"
         }
-        
+
+        welcome.textContent = ``;
         playerChoice.textContent = ``;
         compChoice.textContent = ``;
         roundResults.textContent = ``;
-
     }
+
+}
+
+//Resets the game UI and player score to init
+function reset() {
+    console.log('reset')
+    playerScore = 0;
+    computerScore = 0;
+    welcome.textContent = `Welcome to Rock Paper Scissors, click Rock, Paper or Scissors to start.`;
+    playerChoice.textContent = ``;
+    compChoice.textContent = ``;
+    roundResults.textContent = ``;
+    playerScoreUI.textContent = ``;
+    computerScoreUI.textContent = ``;
+    gameResults.textContent = ``;
 }
